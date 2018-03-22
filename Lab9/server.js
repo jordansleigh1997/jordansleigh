493b31851d0cc2c738dbe2ee3fd49af5da00be76
JORDAN SLEIGH (1504085) <1504085@rgu.ac.uk>
2018-03-22 14:17:09 +0000

    Lab9

--- Lab9/server.js
+++ Lab9/server.js
@@ -8,3 +8,9 @@ app.listen(8080);
 app.get('/test', function(req, res){
  res.send("this is route 2");
 });
+
+app.get('/add', function(req, res){
+var x = req.query.x;
+var y = req.query.y;
+ res.send("X + Y="+(x+y));
+});
